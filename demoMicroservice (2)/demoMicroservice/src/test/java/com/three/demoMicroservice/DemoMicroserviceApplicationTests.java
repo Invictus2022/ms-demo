bdcf733f2329608c9d850be89f97e456e78ab351
hritik <rohan.kinshu@gmail.com>
2024-08-20 15:15:58 +0530

    Testing

---
+++ demoMicroservice (2)/demoMicroservice/src/test/java/com/three/demoMicroservice/DemoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three.demoMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
