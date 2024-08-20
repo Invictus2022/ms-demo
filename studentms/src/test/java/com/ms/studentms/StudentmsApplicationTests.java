f05ee36999d96df6e0de2145ded0237a46df0211
hritik <rohan.kinshu@gmail.com>
2024-08-20 18:18:24 +0530

    Add student app

---
+++ studentms/src/test/java/com/ms/studentms/StudentmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.studentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
